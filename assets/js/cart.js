4c1361fd0b4669d8e7e18f569d17d760645c112d
Osman Ibrahim <oselibas@gmail.com>
2025-06-05 10:01:00 -0700

    Update cart.js
    - fixing issue on adding extra items to cart

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
 
         $.ajax({
-            url: '<?= $base_path ?>includes/cart.inc.php',
+            url: basePath + 'includes/cart.inc.php',
             method: 'POST',
             data: {
                 'add_item': true,
